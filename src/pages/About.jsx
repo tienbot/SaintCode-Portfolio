81b1e1d9d0b9b9467b814157ed23689efa023556
Minakli <ges-a@ya.ru>
2023-05-17 16:31:54 +0530

    About has been created.

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,18 @@
 import style from "./About.module.css";
 
 const About = () => {
-  return <div></div>;
+  return (
+    <main>
+      <div className={style.sidebar}></div>
+      <div className={style.menu__wrapper}></div>
+      <div className={style.content__wrapper_left}>
+        <div className={style.separator}></div>
+      </div>
+      <div className={style.content__wrapper_right}>
+        <div className={style.separator}></div>
+      </div>
+    </main>
+  );
 };
 
 export default About;
