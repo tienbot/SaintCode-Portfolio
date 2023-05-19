11638e76afa8aa9249e81167c9a0f6b2a848f93c
ilia <iliajuso@gmail.com>
2023-05-19 19:53:09 +0400

    add card

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -5,19 +5,38 @@ import { Button } from '../Button/Button'
 import { IconColor } from '../IconColor/IconColor'
 import { IconBW} from '../IconBW/IconBW'
 
-export const Card = () => {
+
+// export const Card = () => {
+//   return (
+//     <div className={s.card}>
+//         <div className={s.card__top}>
+//             <img className={s.card__img} src={img} alt="изображение проекта" />
+//             <IconColor vue alt='icon'/>
+//         </div>
+//         <div className={s.card__bottom}>
+//             <p>Duis aute irure dolor in velit esse cillum dolore.</p>
+//             <Button children='view-project' ghost/>
+//             <IconBW css active alt='icon'/>
+//             <IconBW vue active alt='icon'/>
+//         </div>
+//     </div>
+//   )
+// }
+export const Card = ({ name, imageUrl, link }) => {
   return (
     <div className={s.card}>
-        <div className={s.card__top}>
-            <img className={s.card__img} src={img} alt="изображение проекта" />
-            <IconColor vue alt='icon'/>
-        </div>
-        <div className={s.card__bottom}>
-            <p>Duis aute irure dolor in velit esse cillum dolore.</p>
-            <Button children='view-project' ghost/>
-            <IconBW css active alt='icon'/>
-            <IconBW vue active alt='icon'/>
-        </div>
+      <h2 className={s.h2}>Projects <span className={s.span}>// _{name}</span></h2>
+      <div className={s.card__top}>
+        <img className={s.card__img} src={imageUrl} alt="изображение проекта" />
+        <IconColor vue alt='icon' />
+      </div>
+      <div className={s.card__bottom}>
+        <p>{name}</p>
+        <a href={link} target="_blank" rel="noopener noreferrer">
+          <Button children='view-project' />
+        </a>
+      </div>
     </div>
-  )
-}
+  );
+};
+
