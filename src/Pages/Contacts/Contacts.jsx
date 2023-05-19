8ca11361c0bba8f08abe7269519df317643b8a53
veresmar <veres.maria.dev@gmail.com>
2023-05-19 19:09:11 +0400

    contacts form

---
+++ src/Pages/Contacts/Contacts.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import s from './Contacts.module.scss'
+import { Form } from '../../components/Form/Form'
+
+export const Contacts = () => {
+  return (
+    <div>
+      <Form/>
+    </div>
+    
+  )
+}
