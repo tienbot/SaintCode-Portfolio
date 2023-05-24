79e9aa0f83e1691aed9a82168bf391f264be9e4c
tienbot1 <tien.ru@mail.ru>
2023-05-24 19:59:47 +0900

    adaptive

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -15,7 +15,7 @@ import { IconBW} from '../IconBW/IconBW'
 //         </div>
 //         <div className={s.card__bottom}>
 //             <p>Duis aute irure dolor in velit esse cillum dolore.</p>
-//             <Button children='view-project' ghost/>
+//             <Button textBtn='view-project' ghost/>
 //             <IconBW css active alt='icon'/>
 //             <IconBW vue active alt='icon'/>
 //         </div>
@@ -25,18 +25,6 @@ import { IconBW} from '../IconBW/IconBW'
 export const Card = ({ name, imageUrl, link }) => {
   return (
     <div className={s.card}>
-<<<<<<< HEAD
-        <div className={s.card__top}>
-            <img className={s.card__img} src={img} alt="изображение проекта" />
-            <IconColor js alt='icon'/>
-        </div>
-        <div className={s.card__bottom}>
-            <p>Duis aute irure dolor in velit esse cillum dolore.</p>
-            <Button children='view-project' ghost/>
-            <IconBW css active alt='icon'/>
-            <IconBW vue active alt='icon'/>
-        </div>
-=======
       <h2 className={s.h2}>Projects <span className={s.span}>// _{name}</span></h2>
       <div className={s.card__top}>
         <img className={s.card__img} src={imageUrl} alt="изображение проекта" />
@@ -45,11 +33,9 @@ export const Card = ({ name, imageUrl, link }) => {
       <div className={s.card__bottom}>
         <p>{name}</p>
         <a href={link} target="_blank" rel="noopener noreferrer">
-          <Button children='view-project' />
+          <Button textBtn='view-project' />
         </a>
       </div>
->>>>>>> 96a2da69a2e2fb5768c6aefc1e3f8eae6b007d8b
     </div>
   );
 };
-
