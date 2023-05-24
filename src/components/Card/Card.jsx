990979cc53198bb41f3666a4989b5104bed78366
ilia <iliajuso@gmail.com>
2023-05-24 20:12:21 +0400

    add clear

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -7,7 +7,7 @@ import { IconBW} from '../IconBW/IconBW'
 
 export const Card = ({ name, imageUrl, link, isVisible, index }) => {
   const [showCard, setShowCard] = useState(false);
- const getIconsByProjectName = (projectName) => {
+  const getIconsByProjectName = (projectName) => {
     const iconsByProject = {
       'denis-novik': ['html', 'css'],
       'dodo-pizza': ['html', 'css'],
@@ -27,7 +27,7 @@ export const Card = ({ name, imageUrl, link, isVisible, index }) => {
   };
 
   const projectIcons = getIconsByProjectName(name);
-   useEffect(() => {
+  useEffect(() => {
     if (isVisible) {
       setShowCard(true);
     }
@@ -39,63 +39,26 @@ export const Card = ({ name, imageUrl, link, isVisible, index }) => {
   }
   return (
     <div className="">
-        <div className="">
-            <h2 className={s.h2}>Projects {index} <span className={s.span}>// _{name}</span></h2>
-        </div>
-        <div className={`${s.card} ${showCard ? s.show : ''}`}>
+      <div className="">
+        <h2 className={s.h2}>Projects {index} <span className={s.span}>// _{name}</span></h2>
+      </div>
+      <div className={`${s.card} ${showCard ? s.show : ''}`}>
      
-            <div className={s.card__top}>
-                <img className={s.card__img} src={imageUrl} alt="изображение проекта" />
-                <div className={s.iconsContainer}>
-                    {projectIcons.map((icon) => (
-                        <IconColor key={icon} language={icon} alt='icon' />
-                    ))}
-                </div>
-            </div>
-            <div className={s.card__bottom}>
-                <p>{name}</p>
-                <a href={link} target="_blank" rel="noopener noreferrer">
-                    <Button textBtn='view-project' />
-                </a>
-            </div>
+        <div className={s.card__top}>
+          <img className={s.card__img} src={imageUrl} alt="изображение проекта" />
+          <div className={s.iconsContainer}>
+            {projectIcons.map((icon) => (
+              <IconColor key={icon} language={icon} alt='icon' />
+            ))}
+          </div>
         </div>
+        <div className={s.card__bottom}>
+          <p>{name}</p>
+          <a href={link} target="_blank" rel="noopener noreferrer">
+            <Button textBtn='view-project' />
+          </a>
+        </div>
+      </div>
     </div>
-    // <div className={s.card}>
-<<<<<<< HEAD
-     <div className={`${s.card} ${showCard ? s.show : ''}`}>
-     <h2 className={s.h2}>Projects {index} <span className={s.span}>// _{name}</span></h2>
-      <div className={s.card__top}>
-        <img className={s.card__img} src={imageUrl} alt="изображение проекта" />
-       <div className={s.iconsContainer}>
-  {projectIcons.map((icon) => (
-    <IconColor key={icon} language={icon} alt='icon' />
-  ))}
-</div>
-      </div>
-      <div className={s.card__bottom}>
-        <p>{name}</p>
-        <a href={link} target="_blank" rel="noopener noreferrer">
-          <Button textBtn='view-project' />
-        </a>
-      </div>
-</div>
-=======
->>>>>>> main
-  );
-};
-// export const Card = () => {
-//   return (
-//     <div className={s.card}>
-//         <div className={s.card__top}>
-//             <img className={s.card__img} src={img} alt="изображение проекта" />
-//             <IconColor vue alt='icon'/>
-//         </div>
-//         <div className={s.card__bottom}>
-//             <p>Duis aute irure dolor in velit esse cillum dolore.</p>
-//             <Button textBtn='view-project' ghost/>
-//             <IconBW css active alt='icon'/>
-//             <IconBW vue active alt='icon'/>
-//         </div>
-//     </div>
-//   )
-// }
+  )
+}
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -107,6 +107,69 @@
 //   );
 // };
 
+// import React, { useEffect, useState } from 'react';
+// import Toggle from '../Toggle/Toogle';
+// import { Check } from '../Check/Check';
+// import s from './Projects.module.sass';
+// import { Card } from '../Card/Card';
+// import { ButtonX } from '../ButtonX/ButtonX';
+
+// export const Projects = () => {
+//   const [projects, setProjects] = useState([]);
+//   const [checkedProjects, setCheckedProjects] = useState([]);
+//   const [buttonXVisible, setButtonXVisible] = useState(false);
+
+//   useEffect(() => {
+//     fetch('https://api.github.com/users/iliajuso/repos')
+//       .then((response) => response.json())
+//       .then((data) => {
+//         setProjects(data);
+//       })
+//       .catch((error) => {
+//         console.error('Error fetching projects:', error);
+//       });
+//   }, []);
+
+//   const handleCheckedProjectsChange = (newCheckedProjects) => {
+//     setCheckedProjects(newCheckedProjects);
+//     setButtonXVisible(true); // Show ButtonX when projects are selected
+//   };
+
+//   const handleButtonXClick = () => {
+//     setCheckedProjects([]); // Clear selected projects
+//     setButtonXVisible(false); // Hide ButtonX
+//   };
+
+//   const filteredProjects = projects.filter((project) =>
+//     checkedProjects.includes(project.name.toLowerCase())
+//   );
+
+//   return (
+//     <main className={s.main}>
+//       <div className={s.wrapper}>
+//         <Toggle dropdownContent={<Check setCheckedProjects={handleCheckedProjectsChange} />}>
+//           <label className={s.labelText}>projects</label>
+//         </Toggle>
+//       </div>
+//        <div className={s.border}>
+//         <ButtonX />
+//        </div>
+//       <hr/>
+//       <div className={s.card_div}>
+//         {filteredProjects.map((project, index) => (
+//           <Card
+//             key={project.id}
+//             name={project.name}
+//             imageUrl={project.owner.avatar_url}
+//             link={project.html_url}
+//             isVisible={checkedProjects.includes(project.name.toLowerCase())}
+//             index={index + 1} // Add the project index prop
+//           />
+//         ))}
+//       </div>
+//     </main>
+//   );
+// };
 import React, { useEffect, useState } from 'react';
 import Toggle from '../Toggle/Toogle';
 import { Check } from '../Check/Check';
@@ -118,6 +181,7 @@ export const Projects = () => {
   const [projects, setProjects] = useState([]);
   const [checkedProjects, setCheckedProjects] = useState([]);
   const [buttonXVisible, setButtonXVisible] = useState(false);
+  const [buttonXText, setButtonXText] = useState('');
 
   useEffect(() => {
     fetch('https://api.github.com/users/iliajuso/repos')
@@ -147,14 +211,14 @@ export const Projects = () => {
   return (
     <main className={s.main}>
       <div className={s.wrapper}>
-        <Toggle dropdownContent={<Check setCheckedProjects={handleCheckedProjectsChange} />}>
+        <Toggle dropdownContent={<Check setCheckedProjects={handleCheckedProjectsChange} setButtonXText={setButtonXText} />}>
           <label className={s.labelText}>projects</label>
         </Toggle>
       </div>
-       <div className={s.border}>
-        <ButtonX />
-       </div>
-      <hr/>
+      <div className={s.border}>
+        {buttonXVisible && <ButtonX onClick={handleButtonXClick}>{buttonXText}</ButtonX>}
+      </div>
+      {/* <hr/> */}
       <div className={s.card_div}>
         {filteredProjects.map((project, index) => (
           <Card
@@ -170,6 +234,5 @@ export const Projects = () => {
     </main>
   );
 };
-
 
 
