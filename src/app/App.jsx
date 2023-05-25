67a96357453ef1745fa1c948b267ac255fe24f39
Minakli <ges-a@ya.ru>
2023-05-25 13:28:40 +0530

    Snippet has been added.

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -10,13 +10,14 @@ import Info from "../components/info/Info";
 import personalInfo from "../components/info/personal_info";
 
 function App() {
-  // const { title, description, content } = personalInfo;
-
   // useRef for Info left div
   const infoRef = useRef(null);
 
   // Array lines for Info left div content
   const [infoLinesArr, setInfoLinesArr] = useState([]);
+
+  // Array code snippets
+  const [codeSnippetArr, setCodeSnippetArr] = useState([]);
 
   // Dividing Info left div content into lines
   useEffect(() => {
@@ -24,7 +25,7 @@ function App() {
       let infoBoxWidth = infoRef.current.getBoundingClientRect().width;
 
       // Width for one letter
-      let signWidth = 21;
+      let signWidth = 16;
 
       const text = personalInfo.content;
       let words = text.split(" ");
@@ -34,7 +35,7 @@ function App() {
       for (let i = 0; i < words.length; i++) {
         let word = words[i];
         let wordWidth = word.length * signWidth;
-        if (currentWidth + wordWidth <= infoBoxWidth) {
+        if (currentWidth + wordWidth <= infoBoxWidth - 140) {
           currentLine += word += " ";
           currentWidth += wordWidth + signWidth;
         } else {
@@ -47,11 +48,22 @@ function App() {
       setInfoLinesArr(updatedLine);
     }
   }, []);
+  useEffect(() => {
+    fetch("https://api.github.com/repos/tienbot/SaintCode-Portfolio/contents")
+      .then((response) => response.json())
+      .then((data) => {
+        setCodeSnippetArr(data);
+      })
+      .catch((error) => {
+        // Обработка ошибок
+        console.error(error);
+      });
+  }, []);
   return (
     <Layout>
       <Header />
       {/* <PageHello/> */}
-      <About>
+      <About codeSnippetArr={codeSnippetArr}>
         <Info
           infoRef={infoRef}
           infoLinesArr={infoLinesArr}
